bb5861f46519b1320d9ef72899589f8ea9af3e15
Tony Theodoropoulos <theodot@outlook.com>
2020-04-13 09:50:59 -0400

    Updated GUI

--- EmployeeRecords/Form1.Designer.cs
+++ EmployeeRecords/Form1.Designer.cs
@@ -49,109 +49,109 @@
             // 
             this.titleLabel.AutoSize = true;
             this.titleLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(18, 14);
+            this.titleLabel.Location = new System.Drawing.Point(16, 11);
             this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(498, 74);
+            this.titleLabel.Size = new System.Drawing.Size(416, 62);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "Employee Records";
             // 
             // idLabel
             // 
             this.idLabel.AutoSize = true;
-            this.idLabel.Location = new System.Drawing.Point(27, 146);
+            this.idLabel.Location = new System.Drawing.Point(75, 115);
             this.idLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.idLabel.Name = "idLabel";
-            this.idLabel.Size = new System.Drawing.Size(109, 20);
+            this.idLabel.Size = new System.Drawing.Size(95, 17);
             this.idLabel.TabIndex = 1;
             this.idLabel.Text = "Employee ID#";
             // 
             // fnLabel
             // 
             this.fnLabel.AutoSize = true;
-            this.fnLabel.Location = new System.Drawing.Point(27, 205);
+            this.fnLabel.Location = new System.Drawing.Point(75, 162);
             this.fnLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.fnLabel.Name = "fnLabel";
-            this.fnLabel.Size = new System.Drawing.Size(86, 20);
+            this.fnLabel.Size = new System.Drawing.Size(76, 17);
             this.fnLabel.TabIndex = 2;
             this.fnLabel.Text = "First Name";
             // 
             // lnLabel
             // 
             this.lnLabel.AutoSize = true;
-            this.lnLabel.Location = new System.Drawing.Point(27, 262);
+            this.lnLabel.Location = new System.Drawing.Point(75, 208);
             this.lnLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnLabel.Name = "lnLabel";
-            this.lnLabel.Size = new System.Drawing.Size(86, 20);
+            this.lnLabel.Size = new System.Drawing.Size(76, 17);
             this.lnLabel.TabIndex = 3;
             this.lnLabel.Text = "Last Name";
             // 
             // dateLabel
             // 
             this.dateLabel.AutoSize = true;
-            this.dateLabel.Location = new System.Drawing.Point(27, 312);
+            this.dateLabel.Location = new System.Drawing.Point(75, 248);
             this.dateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dateLabel.Name = "dateLabel";
-            this.dateLabel.Size = new System.Drawing.Size(83, 20);
+            this.dateLabel.Size = new System.Drawing.Size(72, 17);
             this.dateLabel.TabIndex = 4;
             this.dateLabel.Text = "Start Date";
             // 
             // salaryLabel
             // 
             this.salaryLabel.AutoSize = true;
-            this.salaryLabel.Location = new System.Drawing.Point(27, 371);
+            this.salaryLabel.Location = new System.Drawing.Point(75, 295);
             this.salaryLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.salaryLabel.Name = "salaryLabel";
-            this.salaryLabel.Size = new System.Drawing.Size(107, 20);
+            this.salaryLabel.Size = new System.Drawing.Size(96, 17);
             this.salaryLabel.TabIndex = 5;
             this.salaryLabel.Text = "Annual Salary";
             // 
             // idInput
             // 
-            this.idInput.Location = new System.Drawing.Point(220, 142);
-            this.idInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.idInput.Location = new System.Drawing.Point(247, 112);
+            this.idInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.idInput.Name = "idInput";
-            this.idInput.Size = new System.Drawing.Size(146, 26);
+            this.idInput.Size = new System.Drawing.Size(130, 22);
             this.idInput.TabIndex = 6;
             // 
             // fnInput
             // 
-            this.fnInput.Location = new System.Drawing.Point(220, 205);
-            this.fnInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.fnInput.Location = new System.Drawing.Point(247, 162);
+            this.fnInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.fnInput.Name = "fnInput";
-            this.fnInput.Size = new System.Drawing.Size(292, 26);
+            this.fnInput.Size = new System.Drawing.Size(130, 22);
             this.fnInput.TabIndex = 7;
             // 
             // lnInput
             // 
-            this.lnInput.Location = new System.Drawing.Point(220, 262);
-            this.lnInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lnInput.Location = new System.Drawing.Point(247, 208);
+            this.lnInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lnInput.Name = "lnInput";
-            this.lnInput.Size = new System.Drawing.Size(292, 26);
+            this.lnInput.Size = new System.Drawing.Size(130, 22);
             this.lnInput.TabIndex = 8;
             // 
             // dateInput
             // 
-            this.dateInput.Location = new System.Drawing.Point(220, 312);
-            this.dateInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dateInput.Location = new System.Drawing.Point(247, 248);
+            this.dateInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateInput.Name = "dateInput";
-            this.dateInput.Size = new System.Drawing.Size(146, 26);
+            this.dateInput.Size = new System.Drawing.Size(130, 22);
             this.dateInput.TabIndex = 9;
             // 
             // salaryInput
             // 
-            this.salaryInput.Location = new System.Drawing.Point(220, 366);
-            this.salaryInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.salaryInput.Location = new System.Drawing.Point(247, 291);
+            this.salaryInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.salaryInput.Name = "salaryInput";
-            this.salaryInput.Size = new System.Drawing.Size(146, 26);
+            this.salaryInput.Size = new System.Drawing.Size(130, 22);
             this.salaryInput.TabIndex = 10;
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(32, 443);
-            this.addButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.addButton.Location = new System.Drawing.Point(247, 342);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(140, 42);
+            this.addButton.Size = new System.Drawing.Size(130, 34);
             this.addButton.TabIndex = 11;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -159,10 +159,10 @@
             // 
             // listButton
             // 
-            this.listButton.Location = new System.Drawing.Point(32, 534);
-            this.listButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listButton.Location = new System.Drawing.Point(28, 427);
+            this.listButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listButton.Name = "listButton";
-            this.listButton.Size = new System.Drawing.Size(141, 42);
+            this.listButton.Size = new System.Drawing.Size(125, 34);
             this.listButton.TabIndex = 13;
             this.listButton.Text = "List";
             this.listButton.UseVisualStyleBackColor = true;
@@ -170,10 +170,10 @@
             // 
             // removeButton
             // 
-            this.removeButton.Location = new System.Drawing.Point(180, 443);
-            this.removeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.removeButton.Location = new System.Drawing.Point(332, 426);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(141, 42);
+            this.removeButton.Size = new System.Drawing.Size(125, 34);
             this.removeButton.TabIndex = 14;
             this.removeButton.Text = "Remove";
             this.removeButton.UseVisualStyleBackColor = true;
@@ -183,17 +183,17 @@
             // 
             this.outputLabel.BackColor = System.Drawing.Color.White;
             this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.outputLabel.Location = new System.Drawing.Point(32, 580);
+            this.outputLabel.Location = new System.Drawing.Point(28, 464);
             this.outputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(482, 190);
+            this.outputLabel.Size = new System.Drawing.Size(429, 152);
             this.outputLabel.TabIndex = 15;
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(544, 785);
+            this.ClientSize = new System.Drawing.Size(484, 628);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.removeButton);
             this.Controls.Add(this.listButton);
@@ -209,7 +209,7 @@
             this.Controls.Add(this.fnLabel);
             this.Controls.Add(this.idLabel);
             this.Controls.Add(this.titleLabel);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "mainForm";
             this.Text = "Add/Delete";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.mainForm_FormClosed);
